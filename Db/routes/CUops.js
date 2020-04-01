475574641c8c765fe9ca224f18405f00c2fcdd5b
shivam-chouhan <shivam.chouhan@sourcefuse.com>
2020-04-01 09:40:48 +0530

    same

--- Db/routes/CUops.js
+++ Db/routes/CUops.js
@@ -3,14 +3,14 @@ import { pool } from "../server.js";
 export let router = express.Router();
 router.post('/savedata', (req, res) => {
     let data = req.body;
-    pool.query(`INSERT INTO user_info(first_name , middle_name ,last_name, email, phone, role ,address )values('${data.first_name}','${data.middle_name}','${data.last_name}','${data.email}','${data.phone}','${data.role}','${data.address}')`);
+    pool.query(`INSERT INTO users(first_name , middle_name ,last_name, email, phone, role ,address )values('${data.first_name}','${data.middle_name}','${data.last_name}','${data.email}','${data.phone}','${data.role}','${data.address}')`);
     res.send('success');
     res.status(200);
 });
 router.post('/updateuser:id', (req, res) => {
     const updMember = req.body;
     pool.query(`
-  UPDATE user_info
+  UPDATE users
   SET(first_name, middle_name, last_name, email, phone, role, address)=('${updMember.first_name}','${updMember.middle_name}','${updMember.last_name}','${updMember.email}',' ${updMember.phone}','${updMember.role}','${updMember.address}')
     WHERE 
     id = ${req.params.id};
--- Db/routes/CUops.ts
+++ Db/routes/CUops.ts
@@ -7,7 +7,7 @@ export let router = express.Router();
 
 router.post('/savedata',(req , res)=>{
   let data = req.body;
-  pool.query(`INSERT INTO user_info(first_name , middle_name ,last_name, email, phone, role ,address )values('${data.first_name}','${data.middle_name}','${data.last_name}','${data.email}','${data.phone}','${data.role}','${data.address}')`)
+  pool.query(`INSERT INTO users(first_name , middle_name ,last_name, email, phone, role ,address )values('${data.first_name}','${data.middle_name}','${data.last_name}','${data.email}','${data.phone}','${data.role}','${data.address}')`)
   res.send('success');
   res.status(200)
 })
@@ -17,7 +17,7 @@ router.post('/savedata',(req , res)=>{
 router.post('/updateuser:id', (req , res)=>{
   const updMember:DataType = req.body;
   pool.query(`
-  UPDATE user_info
+  UPDATE users
   SET(first_name, middle_name, last_name, email, phone, role, address)=('${updMember.first_name}','${updMember.middle_name}','${updMember.last_name}','${updMember.email}',' ${updMember.phone}','${updMember.role}','${updMember.address}')
     WHERE 
     id = ${req.params.id};
